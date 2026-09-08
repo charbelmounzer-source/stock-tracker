87c60980438e6464d2f350a6528c327f3f6cca14
Charbel-Mounzer_niq <charbel.mounzer@nielseniq.com>
2026-09-08 14:39:31 +0300

    Fix bq_client to handle missing secrets locally

--- bq_client.py
+++ bq_client.py
@@ -1,17 +1,25 @@
 import os
-import streamlit as st
 from dotenv import load_dotenv
 from google.cloud import bigquery
-from google.oauth2 import service_account
 
 load_dotenv()
 
-if "gcp_service_account" in st.secrets:
+def _try_get_streamlit_secrets():
+    try:
+        import streamlit as st
+        if "gcp_service_account" in st.secrets:
+            return st.secrets["gcp_service_account"]
+    except Exception:
+        pass
+    return None
+
+gcp_secrets = _try_get_streamlit_secrets()
+
+if gcp_secrets:
     # Running on Streamlit Cloud -- use secrets
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"]
-    )
-    project_id = st.secrets["gcp_service_account"]["project_id"]
+    from google.oauth2 import service_account
+    credentials = service_account.Credentials.from_service_account_info(gcp_secrets)
+    project_id = gcp_secrets["project_id"]
     bq_client = bigquery.Client(project=project_id, credentials=credentials)
 else:
     # Running locally -- use the .env file path
